f6d9002484f2e19709727b030e6c95d6d5e5947d
LBEnverus <144834875+LBEnverus@users.noreply.github.com>
2023-12-28 13:23:30 -0800

    Add files via upload

---
+++ swagger-initializer.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "https://gist.githubusercontent.com/LBEnverus/5fd59f24d83c2aa7c42d40603499131d/raw/39b1c8d313280e7d12561654ea756ae0fad8aeab/constraint-db-swagger.yaml",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
