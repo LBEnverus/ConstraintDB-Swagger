f45a31870fa0a5a64b739c85e03ad8c5fe173444
LBEnverus <144834875+LBEnverus@users.noreply.github.com>
2024-04-09 11:03:19 -0500

    Update swagger-initializer.js

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://gist.githubusercontent.com/LBEnverus/5fd59f24d83c2aa7c42d40603499131d/raw/0d8f5820fa4b94f88b78221ec5e4477d89903457/constraintdb-swagger.yaml",
+    url: "https://gist.githubusercontent.com/LBEnverus/5fd59f24d83c2aa7c42d40603499131d/raw/b0a99c525b9173be0646e7f7c05f5de5149d1934/constraintdb-swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
